e6b3387885f5b39d16e68ee94da6094da5c477dd
Andre Carlucci <andrecarlucci@gmail.com>
2014-02-13 15:43:00 -0200

    Column.Clob for ReversableSchemaMigration

--- Sharp.Migrations/ReversableSchemaMigration.cs
+++ Sharp.Migrations/ReversableSchemaMigration.cs
@@ -28,6 +28,10 @@ namespace Sharp.Migrations {
             }
             public static FluentColumn String(string name) { return new FluentColumn(name, DbType.String); }
             public static FluentColumn String(string name, int size) { return new FluentColumn(name, DbType.String, size); }
+
+            public static FluentColumn Clob(string name) {
+                return Data.Schema.Column.Clob(name);
+            }
             public static FluentColumn Int16(string name) { return new FluentColumn(name, DbType.Int16); }
             public static FluentColumn Int32(string name) { return new FluentColumn(name, DbType.Int32); }
             public static FluentColumn Int64(string name) { return new FluentColumn(name, DbType.Int64); }
