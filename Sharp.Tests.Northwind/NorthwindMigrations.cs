0dccda77aa26c37af3c1f52bcc5f99618b23890d
andrecarlucci <andrecarlucci@gmail.com>
2017-05-22 22:48:38 -0300

    deleting old project

--- Sharp.Tests.Northwind/NorthwindMigrations.cs
+++
@@ -1,9 +0,0 @@
-﻿using Sharp.Migrations.Runners;
-
-namespace Northwind.Sharp.Migrations {
-    public class NorthwindMigrations : ChooseDbConsoleRunner {
-        public NorthwindMigrations()
-            : base("", "") {
-        }
-    }
-}
